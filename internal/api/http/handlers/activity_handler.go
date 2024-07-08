6d4e3de1fd237e7b2bde6272ee5a74d1a922762b
Daniel Guillen <danielguillencoco@yahoo.com>
2024-07-07 21:50:04 -0500

    add some todo tasks

--- internal/api/http/handlers/activity_handler.go
+++ internal/api/http/handlers/activity_handler.go
@@ -15,6 +15,20 @@ type ActivityHandler struct {
 	Service *service.ActivityService
 }
 
+// TODO: IF THE ACTIVITY ALREADY EXISTS WE UPDATE THE TIME FOR THAT ACTIVITY
+// ??: TRACK THE TIME A USER SPENDS ON VSCODE? make a vscode extension that logs the time spent on vscode
+// ?? HOW CAN WE PREVENT USERS FROM LOGGING ACTIVITIES IN THE FUTURE? OR IN THE PAST?
+// TODO: Implement a check in the app that disallows logging activities with a timestamp later than the current system time.
+// !! Set a threshold for how far back users can log activities (e.g., no more than 24 hours in the past).
+// ?? HOW CAN WE PREVENT ADDING OF HUGE AMOUNTS OF TIME that is not possible?
+// !! Set reasonable limits on the maximum amount of time that can be logged for an activity per day (e.g., a maximum of 16 hours).
+// !! Validate the time input against these limits before saving the activity.
+// ?? HOW CAN WE SOMEHOW BE STRCIKTER WITH THE TIME LOGGED? SHOULD WE ADD NOTES WHEN LOGGING TIME?
+// TODO FIGURE OUT WAYS WE CAN VERIFY THE TIME LOGGED, AND VERIFY THE OUTPUT OF THE TIME LOGGED
+// !! ADD A NOTES FIELD TO THE ACTIVITY MODEL and ask user what they did and their output
+
+// !! FOR THE SCORE - implement feature where the more time you spent grinding the higher the score and the more intense badge you get
+// !! the web app also changes colors based on the score
 func (h *ActivityHandler) LogActivity(c *gin.Context) {
 	var activity models.Activity
 	if err := c.BindJSON(&activity); err != nil {
